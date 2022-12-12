82802f0bc3e39db5dd9905c90fc7f68b306a8d5c
redtime02 <115767377+redtime02@users.noreply.github.com>
2022-12-12 19:51:11 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+// var myRandom = [
+//     'Toan',
+//     'Nhan',
+//     'Uyen',
+//     'Toan'
+
+// ];
+
+// var a = 0;
+// a < 3;
+// a  = Math.ceil(Math.random(a) *3);
+
+// console.log(myRandom[a]);
